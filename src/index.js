85a8b2d4ff92e3edbe253d242ffcfba6a57a5ba2
Bouali <bouali.lemak@gmail.com>
2019-06-27 15:07:53 +0100

    first commit integration of arcgis webpack plugin and showing a map example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
+import './config';
+import Map from "esri/Map";
+import MapView from "esri/views/MapView";
+
 import { Header } from './app/header';
 import '../src/index.scss';
-import webpackgif from './assets/images/webpack.gif';
+// import webpackgif from './assets/images/webpack.gif';
 
 let header = new Header();
-document.getElementById('webpack-gif').setAttribute('src', webpackgif);
+// document.getElementById('webpack-gif').setAttribute('src', webpackgif);
 let firstHeading = header.getFirstHeading();
+console.log(firstHeading)
+
+var map = new Map({
+    basemap: "streets"
+  });
+
+  var view = new MapView({
+    container: "viewDiv",
+    map: map,
+    zoom: 4,
+    center: [15, 65] // longitude, latitude
+  });
--- webpack/webpack.common.config.js
+++ webpack/webpack.common.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ArcGISPlugin = require('@arcgis/webpack-plugin');
 
 
 module.exports = {
@@ -48,19 +49,47 @@ module.exports = {
                     },
 
                 ]
-            }
+            },
+            {
+                test: /\.(wsv|ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                use: [
+                  {
+                    loader: "file-loader",
+                    options: {
+                      name: "build/[name].[ext]"
+                    }
+                  }
+                ]
+              },
+        
         ]
     },
 
     plugins: [
         new CleanWebpackPlugin(),
+        new ArcGISPlugin({
+           useDefaultAssetLoaders: false,
+        //    userDefinedExcludes: [
+        //     "arcgis-js-api/layers/BingMapsLayer",
+        //     "arcgis-js-api/layers/CSVLayer",
+        //     "arcgis-js-api/layers/GeoRSSLayer",
+        //     "arcgis-js-api/layers/ImageryLayer",
+        //     "arcgis-js-api/layers/KMLLayer",
+        //     "arcgis-js-api/layers/MapImageLayer",
+        //     "arcgis-js-api/layers/OpenStreetMapLayer",
+        //     "arcgis-js-api/layers/StreamLayer",
+        //     "arcgis-js-api/layers/WMSLayer",
+        //     "arcgis-js-api/layers/WMTSLayer",
+        //     "arcgis-js-api/layers/WebTileLayer"
+        //   ]
+        }),
         new MiniCssExtractPlugin({
             filename: 'style.[chunkhash].css'
         }),
         new HtmlWebpackPlugin({
             title: 'lifadmin',
             template: './src/index.html',
-            inject: true,
+            chunksSortMode: 'none',
             minify: {
                 removeComments: true,
                 collapseWhitespace: false
@@ -71,7 +100,27 @@ module.exports = {
             to: 'assets/images'
         }]),
 
-        
 
-    ]
+    ],
+ 
+    resolve: {
+        modules: [path.resolve(__dirname, "/src"), "node_modules/"],
+        extensions: [".js", ".scss"]
+      },
+    
+      externals: [
+        (context, request, callback) => {
+          if (/pe-wasm$/.test(request)) {
+            return callback(null, "amd " + request);
+          }
+          callback();
+        }
+      ],
+    
+      node: {
+        process: false,
+        global: false
+      }
+
+
 }
